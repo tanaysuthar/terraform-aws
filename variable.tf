0fb5f9f3c50d637f2fdec898b230e1c996eec4e0
Tanay <tanaysuthar265@gmail.com>
2019-08-05 15:19:01 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,21 +1,21 @@
-variable "count" {
-	default = 1
+variable "countone" {
+        default = 1
 }
 
 
 variable "region" {
-	description = "AWS region for infrastructure"
-	default = "us-east-1"
+        description = "AWS region for infrastructure"
+        default = "us-east-1"
 }
 
 variable "key_name" {
-	description = "Key_name for SSHing into EC2"
-	default = "webapp.pem"
+        description = "Key_name for SSHing into EC2"
+        default = "webapp.pem"
 }
 
 variable "amis" {
-	description = "Base AMI to launch the instance"
-	default = {
-			us-east-1 = "ami-0080e4c5bc078760e"
-	}
+        description = "Base AMI to launch the instance"
+        default = {
+                        us-east-1 = "ami-0080e4c5bc078760e"
+        }
 }
